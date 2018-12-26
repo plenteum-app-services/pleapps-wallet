e0f02d8db6efed3d1f892ccb4cb48ad2d43d8366
Brandon Lehmann <brandonlehmann@gmail.com>
2018-12-26 13:33:33 -0500

    Adding code documentation and quick cleanup of a few items

--- index.js
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2018, TurtlePay Developers
-//
-// Please see the included LICENSE file for more information.
-
-'use strict'
--- walletCreator.js
+++ walletCreator.js
@@ -4,6 +4,7 @@
 
 'use strict'
 
+const Config = require('./config.json')
 const TurtleCoinUtils = require('turtlecoin-utils')
 const request = require('request-promise-native')
 const RabbitMQ = require('amqplib')
@@ -11,9 +12,7 @@ const cluster = require('cluster')
 const util = require('util')
 const cryptoUtils = new TurtleCoinUtils()
 const cpuCount = require('os').cpus().length
-const topBlockUrl = 'https://blockapi.turtlepay.io/block/header/top'
-const creationQueue = 'request.wallet'
-const scanQueue = 'scan.wallet'
+const topBlockUrl = Config.blockHeaderUrl + 'top'
 
 const publicRabbitHost = process.env.RABBIT_PUBLIC_SERVER || 'localhost'
 const publicRabbitUsername = process.env.RABBIT_PUBLIC_USERNAME || ''
@@ -31,6 +30,7 @@ function spawnNewWorker () {
   cluster.fork()
 }
 
+/* Helps us to build the RabbitMQ connection string */
 function buildConnectionString (host, username, password) {
   var result = ['amqp://']
 
@@ -56,47 +56,71 @@ if (cluster.isMaster) {
 } else if (cluster.isWorker) {
   (async function () {
     try {
+      /* Set up our access to the necessary RabbitMQ systems */
       var incoming = await RabbitMQ.connect(buildConnectionString(publicRabbitHost, publicRabbitUsername, publicRabbitPassword))
       var incomingChannel = await incoming.createChannel()
 
       var outgoing = await RabbitMQ.connect(buildConnectionString(privateRabbitHost, privateRabbitUsername, privateRabbitPassword))
       var outgoingChannel = await outgoing.createChannel()
 
-      await incomingChannel.assertQueue(creationQueue)
-      await outgoingChannel.assertQueue(scanQueue, {
+      await incomingChannel.assertQueue(Config.queues.new)
+      await outgoingChannel.assertQueue(Config.queues.scan, {
         durable: true
       })
 
-      incomingChannel.consume(creationQueue, (message) => {
+      /* Let's get any new wallet requests from the public facing API */
+      incomingChannel.consume(Config.queues.new, (message) => {
+        /* Is the message real? */
         if (message !== null) {
+          /* Generate a new address, nothing fancy going on here, just a brand new address */
           var newAddress = cryptoUtils.createNewAddress()
 
+          /* We need to go get some information about the current top block */
           request({
             url: topBlockUrl,
             json: true
           }).then((topBlock) => {
+            /* Now that we know what our scan height is, we can get moving */
             log(util.format('Worker #%s: Created new wallet %s at height %s for %s', cluster.worker.id, newAddress.address, topBlock.height, message.properties.correlationId))
 
-            incomingChannel.sendToQueue(message.properties.replyTo, Buffer.from(newAddress.address), {
+            /* Construct our response back to the public API -- non-sensitive information */
+            const response = {
+              address: newAddress.address,
+              scanHeight: topBlock.height,
+              maxHeight: (topBlock.height + Config.maximumScanBlocks)
+            }
+
+            /* Go ahead and fire that information back to the public API for the related request */
+            incomingChannel.sendToQueue(message.properties.replyTo, Buffer.from(JSON.stringify(response)), {
               correlationId: message.properties.correlationId
             })
 
+            /* Construct our payload for sending via the private RabbitMQ server
+               to the workers that are actually scanning the wallets for incoming
+               funds */
             const scanRequest = {
               wallet: newAddress,
               scanHeight: topBlock.height,
+              maxHeight: (topBlock.height + Config.maximumScanBlocks),
               request: JSON.parse(message.content.toString())
             }
 
-            outgoingChannel.sendToQueue(scanQueue, Buffer.from(JSON.stringify(scanRequest)), { persistent: true })
+            /* Go ahead and send that message to the scanning queue */
+            outgoingChannel.sendToQueue(Config.queues.scan, Buffer.from(JSON.stringify(scanRequest)), { persistent: true })
 
+            /* Acknowledge that we've handled this new wallet request */
             incomingChannel.ack(message)
           }).catch((error) => {
+            /* If we weren't able to retrieve what the current top block is (height) then we're bailing */
             log(util.format('Worker #%s: Could not create new wallet [%s]', cluster.worker.id, error.toString()))
+
+            /* Someone else can handle this request */
             incomingChannel.nack(message)
           })
         }
       })
     } catch (e) {
+      /* Something went horribly wrong and this worker needs to go */
       log(util.format('Error in worker #%s: %s', cluster.worker.id, e.toString()))
       cluster.worker.kill()
     }
